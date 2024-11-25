2b718156a5875f8b44fd4f21747c30ca0f798662
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-24 21:06:46 -0800

    feat- add conn parameter to setupInput

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
-const { connect } = require("./client");
-const { setupInput } = require("./input");
+const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
 
-setupInput();
+setupInput(conn);
