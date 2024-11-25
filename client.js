70fb85a3475dba42b61bca245a7a1efcfdb50280
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-24 16:09:11 -0800

    feat-add rl.question to prompt for username with setTime for proper sequence

--- client.js
+++ client.js
@@ -1,24 +1,52 @@
 const net = require("net");
+const readline = require('readline');
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
 
+  // create interface for reading input from the terminal
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
   // listens and interprets incoming data
   conn.on("data", (data) => {
-    console.log("Message from the Server: ", (data))
-  })
+    console.log("Message from the Server: ", (data));
+  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+  // Informs client that they're connected
+  conn.on("connect", () => {
+    console.log("Successfully connected to server\n");
+  });
+
+  // Use small timeout to ensure messages print in order
+  setTimeout(() => {
+    // Prompt user for in-game username
+    rl.question("Enter your initials or Player name (max 3 characters):\n", (userName) => {
+      // Set your Players name, max. 3 characters
+      const trimmedName = userName.substring(0, 3);
+
+      //Send name to the server
+      conn.write(`Name: ${trimmedName}\n`);
+
+      // Close readline interface
+      rl.close();
+
+      // interpret incoming data as text
+      conn.setEncoding("utf8");
+
+    });
+
+  }, 500);
 
   return conn;
 };
 
-console.log("Connecting ...");
-connect();
+
 
 module.exports = connect;
