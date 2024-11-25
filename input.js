af3dc8b1da0cabd92e897009f3cb364ef5f69089
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-24 21:04:21 -0800

    feat- add Map keys to movement commands, & conn parameter in setupInput

--- input.js
+++ input.js
@@ -1,12 +1,8 @@
-// set up an exit route from program with specific keysS
-const handleUserInput = function(key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
+// setup interface to handle user input from stdin
+let connection;
 
-// setup interface to handle user input from stdin'
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;  // create variable to hold the stdin object so we don't have to type process.stdin multiple times
   stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
   stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
@@ -14,5 +10,26 @@ const setupInput = function() {
   stdin.on('data', handleUserInput); // Listens for specific user input to exit program
   return stdin;   // return the stdin object so we can use it elsewhere in the program
 };
+
+// set up an exit route from program with specific key
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+
+  // Map Keys to movement commands
+  if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  }
+
+};
+
+
 
 module.exports = setupInput;
