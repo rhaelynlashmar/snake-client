4bafefa6ef11f9272d0e1f733ee37668bc704a5a
Rhaelyn L <rhaelynlashmar@gmail.com>
2024-11-24 21:02:19 -0800

    fix bug- readline not closing properly- removed, did conn.write(Name:RAE) instead

--- client.js
+++ client.js
@@ -1,62 +1,34 @@
 const net = require("net");
-const readline = require('readline');
 
 // establishes a connection with the game server
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: "localhost",
-    port: 50541
+    port: 50541,
   });
 
-  // create interface for reading input from the terminal
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  // interpret incoming data as text 
+  conn.setEncoding("utf8");
 
   // listens and interprets incoming data
   conn.on("data", (data) => {
-    console.log("Message from the Server: ", (data));
+    console.log("Message from the Server: ", data);
   });
 
-  // Informs client that they're connected
+  // handle errors on connection
+  conn.on("error", (err) => {
+    console.error("Connection error: ", err.message);
+  });
+
+  // Informs user that they're connected
   conn.on("connect", () => {
     console.log("Successfully connected to server\n");
+    // User is always me, I guess
+    conn.write("Name: RAE"); 
+
   });
-
-  // Use small timeout to ensure messages print in order
-  setTimeout(() => {
-    // Prompt user for in-game username
-    rl.question("Enter your initials or Player name (max 3 characters):\n", (userName) => {
-      // Set your Players name, max. 3 characters
-      const trimmedName = userName.substring(0, 3);
-
-      //Send name to the server
-      conn.write(`Name: ${trimmedName}\n`);
-
-      // Close readline interface
-      rl.close();
-
-      // interpret incoming data as text
-      conn.setEncoding("utf8");
-
-    });
-
-  }, 100);
-
-  // Do another connect callback to send the move command
-  // conn.on("connect", () => {
-  // Send initial move command
-  // conn.write("Move: up");    // conn.write("Move: down");
-  // conn.write("Move: left");
-  // conn.write("Move: right");
-  // });
 
   return conn;
 };
-
-
-
-
 
 module.exports = connect;
